93b7730f9e233314d2a69e64d806f601ce112e51
mohamed-othman <muhammad.oth91@gmail.com>
2017-05-04 12:03:37 +0200

    fix deployment bugs #2

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+app.locals.dir = path.join(__dirname, '/views');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.set('view engine', 'ejs');
+app.set('mainDir',__dirname);
+app.engine('html', require('ejs').renderFile);
+var favicon = require('serve-favicon');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser())
+app.use(require('./routes/index'));
+app.use(require('./routes/users'));
+app.use(require('./routes/emps'));
+app.use(require('./routes/categories'));
+app.use(require('./routes/products'));
+app.use(require('./routes/bills'));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
