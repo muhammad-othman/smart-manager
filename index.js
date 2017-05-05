bb0c79022bfd7794c140bd198e223c2a75c7c564
muhammad-othman <muhammad.oth91@outlook.com>
2017-05-05 10:45:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var favicon = require('serve-favicon');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(cookieParser())
 app.use(require('./routes/index'));
 app.use(require('./routes/users'));
