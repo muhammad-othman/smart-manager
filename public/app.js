d07a9439aef9a3b9fef852596095808fe0595a2f
muhammad-othman <muhammad.oth91@outlook.com>
2017-05-05 10:56:12 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -9,55 +9,55 @@ app.config(function($sceDelegateProvider) {
 });
 app.config(function ($routeProvider) {
     $routeProvider.when("/Products", {
-        templateUrl: "http://" + window.location.host + "/Views/Products.html",
+        templateUrl: "http://" + window.location.host + "/views/Products.html",
         controller: "ProductsController"
     }).when("/AddProducts", {
-        templateUrl: "http://" + window.location.host + "/Views/AddProducts.html",
+        templateUrl: "http://" + window.location.host + "/views/AddProducts.html",
         controller: "AddProductsController"
     }).when("/UpdateProducts/:id", {
-        templateUrl: "http://" + window.location.host + "/Views/UpdateProducts.html",
+        templateUrl: "http://" + window.location.host + "/views/UpdateProducts.html",
         controller: "UpdateProductsController"
     }).when("/ProductsCategories", {
-        templateUrl: "http://" + window.location.host + "/Views/ProductsCategories.html",
+        templateUrl: "http://" + window.location.host + "/views/ProductsCategories.html",
         controller: "ProductsCategoriesController"
     }).when("/AddProductsCategories", {
-        templateUrl: "http://" + window.location.host + "/Views/AddProductsCategories.html",
+        templateUrl: "http://" + window.location.host + "/views/AddProductsCategories.html",
         controller: "ProductsCategoriesController"
     }).when("/AddSellingBill", {
-        templateUrl: "http://" + window.location.host + "/Views/AddSellingBill.html",
+        templateUrl: "http://" + window.location.host + "/views/AddSellingBill.html",
         controller: "SellingBillController"
     }).when("/Employees", {
-        templateUrl: "http://" + window.location.host + "/Views/Employees.html",
+        templateUrl: "http://" + window.location.host + "/views/Employees.html",
         controller: "EmployeesController"
     }).when("/AddEmployees", {
-        templateUrl: "http://" + window.location.host + "/Views/AddEmployees.html",
+        templateUrl: "http://" + window.location.host + "/views/AddEmployees.html",
         controller: "EmployeesController"
     }).when("/UpdateEmployee/:id", {
-        templateUrl: "http://" + window.location.host + "/Views/UpdateEmployee.html",
+        templateUrl: "http://" + window.location.host + "/views/UpdateEmployee.html",
         controller: "UpdateEmployeesController"
     }).when("/UpdateCategory/:id", {
-        templateUrl: "http://" + window.location.host + "/Views/UpdateProductsCategories.html",
+        templateUrl: "http://" + window.location.host + "/views/UpdateProductsCategories.html",
         controller: "UpdateCategoryController"
     }).when("/Users", {
-        templateUrl: "http://" + window.location.host + "/Views/Users.html",
+        templateUrl: "http://" + window.location.host + "/views/Users.html",
         controller: "UsersController"
     }).when("/AddUsers", {
-        templateUrl: "http://" + window.location.host + "/Views/AddUsers.html",
+        templateUrl: "http://" + window.location.host + "/views/AddUsers.html",
         controller: "AddUsersController"
     }).when("/UpdateUser/:id", {
-        templateUrl: "http://" + window.location.host + "/Views/UpdateUsers.html",
+        templateUrl: "http://" + window.location.host + "/views/UpdateUsers.html",
         controller: "UpdateUsersController"
     }).when("/SellingBill", {
-        templateUrl: "http://" + window.location.host + "/Views/SellingBill.html",
+        templateUrl: "http://" + window.location.host + "/views/SellingBill.html",
         controller:"SellingBillController"
     }).when("/BuyingBill", {
-        templateUrl: "http://" + window.location.host + "/Views/BuyingBill.html",
+        templateUrl: "http://" + window.location.host + "/views/BuyingBill.html",
         controller: "BuyingBillController"
     }).when("/AddBuyingBill", {
-        templateUrl: "http://" + window.location.host + "/Views/AddBuyingBill.html",
+        templateUrl: "http://" + window.location.host + "/views/AddBuyingBill.html",
         controller: "BuyingBillController"
     }).when("/Welcome", {
-        templateUrl: "http://" + window.location.host + "/Views/Welcome.html"
+        templateUrl: "http://" + window.location.host + "/views/Welcome.html"
     });
     $routeProvider.otherwise({
         redirectTo: "Welcome"
